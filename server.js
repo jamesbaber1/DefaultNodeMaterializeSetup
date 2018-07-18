ffce3560bcaa5c0f72ef8b05fb22bab87d551f12
Baber <597847@bah.com>
2018-07-18 11:21:04 -0500

    Setup start script heroku port

--- server.js
+++ server.js
@@ -1,52 +1,54 @@
 const express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
+const  hbs = require('hbs');
+const fs = require('fs')
+const port = process.env.PORT || 3000;
 
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials')
+
+hbs.registerPartials(__dirname +'/views/partials')
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
 
-  console.log(log);
-  fs.appendFile('server.log', log + '\n');
-  next();
+
+app.use((req, res, next) => {                               //middleware example: request logger
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log +'\n', (err) => {
+        if(err){
+            console.log('Unable to append to server log.')
+        }
+    })
+    next();
 });
 
 
-app.use(express.static(__dirname + '/public'));
-
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
+hbs.registerHelper('getCurrentYear', ()=>{  //function without an argument
+    return new Date().getFullYear()
 });
 
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
+hbs.registerHelper('screamIt', (text) => {      //function with an argument  hbs syntax:   {{screamIt text}}
+    return text.toUpperCase();
+})
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
+    res.render('home.hbs', {
+        welcomeMessage: 'Welcome to my website!!',
+        pageTitle: 'Home Page'
+    });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
-  });
-});
+app.use(express.static(__dirname + '/public'));
 
-// /bad - send back json with errorMessage
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
-});
-
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, ()=>{
+    console.log(`Server is up on  port ${port}`)
 });
